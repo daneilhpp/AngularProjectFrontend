44f0a619ba885d9e5d77532cf4e87f898b6bf124
daneilhpp <carvalhosilvad88@gmail.com>
2022-06-14 22:08:34 -0300

    [Commit06] Product Commition Addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
 
 registerLocaleData(localePt);
 
@@ -31,7 +33,9 @@ registerLocaleData(localePt);
     AppRoutingModule,
     FormsModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
--- src/app/services/produtos.service.ts
+++ src/app/services/produtos.service.ts
@@ -1,7 +1,9 @@
 import { IProduto } from './../model/IProduto.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,28 @@ import { Observable } from 'rxjs';
 export class ProdutosService {
   private URL: string = 'http://localhost:3000/produtos';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   buscarTodos():Observable<IProduto[]>{
-    return this.http.get<IProduto[]>(this.URL);
+    return this.http.get<IProduto[]>(this.URL).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
+  }
+
+  cadastrar(produto: IProduto):Observable<IProduto>{
+    return this.http.post<IProduto>(this.URL, produto).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
+  }
+
+  exibirErro(e:any):Observable<any>{
+    this.exibirMensagem('Erro!','Não foi possível realizar a operação','toast-error');
+    return EMPTY;
+  }
+
+  exibirMensagem(titulo:string, mensagem:string, tipo:string):void{
+    this.toastr.show(mensagem,titulo,{closeButton:true, progressBar:true},tipo);
   }
 }
